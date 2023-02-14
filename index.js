60498a6a20e6d21e6f3f5c641c9f9e5345fed087
Santiago Santoro <santiagosantoro10@gmail.com>
2023-02-13 22:04:11 -0300

    prueba getElementsByClassName

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-// CARRITO DE COMPRAS DE LA SECCION MERCH//
+// // CARRITO DE COMPRAS DE LA SECCION MERCH//
 
-const productos = [
-    { id: 1, nombre: "remera", precio: "$3500", categoria: "Indumentaria", talle: "M" },
-    { id: 2, nombre: "mochila", precio: "$1500", categoria: "Indumentaria", talle: "M" },
-    { id: 3, nombre: "gorra", precio: "$3500", categoria: "Indumentaria", talle: "M" },
-    { id: 4, nombre: "buzo", precio: "$6000", categoria: "Indumentaria", talle: "M" },
-    { id: 5, nombre: "musculosa", precio: " $ 4500", categoria: "Indumentaria", talle: "M" },
-];
+// const productos = [
+//     { id: 1, nombre: "remera", precio: "$3500", categoria: "Indumentaria", talle: "M" },
+//     { id: 2, nombre: "mochila", precio: "$1500", categoria: "Indumentaria", talle: "M" },
+//     { id: 3, nombre: "gorra", precio: "$3500", categoria: "Indumentaria", talle: "M" },
+//     { id: 4, nombre: "buzo", precio: "$6000", categoria: "Indumentaria", talle: "M" },
+//     { id: 5, nombre: "musculosa", precio: " $ 4500", categoria: "Indumentaria", talle: "M" },
+// ];
 
-// productos.push({ id: 6, nombre: "Medias", precio: "1000", categoria: "Indumentaria" },);
-// productos.unshift({ id: 0, nombre: "Zapatillas", precio: "12000", categoria: "Indumentaria" },);
-// console.log(productos);
+// // productos.push({ id: 6, nombre: "Medias", precio: "1000", categoria: "Indumentaria" },);
+// // productos.unshift({ id: 0, nombre: "Zapatillas", precio: "12000", categoria: "Indumentaria" },);
+// // console.log(productos);
 
 
-while (prompt) {
-    const producto = prompt("Ingrese el nombre del producto que desea comprar:"
-        + "\n1. Remera"
-        + "\n2. Mochila"
-        + "\n3. Gorra"
-        + "\n4. Buzo"
-        + "\n5. Musculosa");
+// while (prompt) {
+//     const producto = prompt("Ingrese el nombre del producto que desea comprar:"
+//         + "\n1. Remera"
+//         + "\n2. Mochila"
+//         + "\n3. Gorra"
+//         + "\n4. Buzo"
+//         + "\n5. Musculosa");
 
-    const filtrado = productos.filter(item => item.nombre === producto.toLowerCase());
+//     const filtrado = productos.filter(item => item.nombre === producto.toLowerCase());
 
-    filtrado.forEach((item) => {
-        let mensaje = `
-    Id: ${item.id}
-    Nombre: ${item.nombre}
-    Precio: ${item.precio}
-    Categoria: ${item.categoria}
-    Talle: ${item.talle}
-    `; alert(mensaje);
-    })
-}
-const cards = document.getElementsByClassName("card");
+//     filtrado.forEach((item) => {
+//         let mensaje = `
+//     Id: ${item.id}
+//     Nombre: ${item.nombre}
+//     Precio: ${item.precio}
+//     Categoria: ${item.categoria}
+//     Talle: ${item.talle}
+//     `; alert(mensaje);
+//     })
+// }
+const card = document.getElementsByClassName("card");
 console.log(card)
 
 
