647dcceedb6d5a2e764a1b3df66cad3d4aa708d6
Santiago Santoro <santiagosantoro10@gmail.com>
2023-02-16 16:37:30 -0300

    cambios

--- index.js
+++ index.js
@@ -17,34 +17,24 @@ const productoIndex = parseInt(prompt("Ingrese el número de ID del producto que
 
 const productoSeleccionado = productos.filter(producto => producto.id === (productoIndex + 1))[0];
 
-let cantidad = parseInt(prompt("Ingrese la cantidad de productos que desea comprar:"));
-while (isNaN(cantidad) || cantidad <= 0) {
-    cantidad = parseInt(prompt("La cantidad ingresada no es válida. Por favor, ingrese un número entero mayor a cero:"));
-}
-
-const productoConCantidad = {
-    producto: productoSeleccionado,
-    cantidad: cantidad
-};
-
 let mensaje = `
-    Id: ${productoConCantidad.producto.id}
-    Nombre: ${productoConCantidad.producto.nombre}
-    Precio: ${productoConCantidad.producto.precio}
-    Cantidad: ${productoConCantidad.cantidad}
-    Categoria: ${productoConCantidad.producto.categoria}
-    Talle: ${productoConCantidad.producto.talle}
+    Id: ${productoSeleccionado.id}
+    Nombre: ${productoSeleccionado.nombre}
+    Precio: ${productoSeleccionado.precio}
+    Categoria: ${productoSeleccionado.categoria}
+    Talle: ${productoSeleccionado.talle}
     `;
 alert(mensaje);
 
-const confirmaCompra = confirm("¿Desea comprarlo?")
+const confirmaCompra = confirm ("¿Desea comprarlo?")
 if (confirmaCompra) {
-    carrito.push(productoConCantidad);
+    carrito.push(productoSeleccionado);
     console.log(carrito);
     alert("Producto ingresado al carrito");
-} else {
+} else { &&
     alert("Compra cancelada");
 }
+
 
 
 
