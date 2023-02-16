d62bb4b2505902ed8b44e3e588ef1deee17f949b
Santiago Santoro <santiagosantoro10@gmail.com>
2023-02-16 14:47:09 -0300

    cambios segunda reentrega

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
 // CARRITO DE COMPRAS DE LA SECCION MERCH//
 
 const productos = [
-    { id: 1, nombre: "remera", precio: "$3500", categoria: "Indumentaria", talle: "M" },
-    { id: 2, nombre: "mochila", precio: "$1500", categoria: "Indumentaria", talle: "M" },
-    { id: 3, nombre: "gorra", precio: "$3500", categoria: "Indumentaria", talle: "M" },
-    { id: 4, nombre: "buzo", precio: "$6000", categoria: "Indumentaria", talle: "M" },
-    { id: 5, nombre: "musculosa", precio: " $ 4500", categoria: "Indumentaria", talle: "M" },
+    { id: 1, nombre: "Remera", precio: "$3500", categoria: "Indumentaria", talle: "M" },
+    { id: 2, nombre: "Mochila", precio: "$1500", categoria: "Indumentaria", talle: "M" },
+    { id: 3, nombre: "Gorra", precio: "$3500", categoria: "Indumentaria", talle: "M" },
+    { id: 4, nombre: "Buzo", precio: "$6000", categoria: "Indumentaria", talle: "M" },
+    { id: 5, nombre: "Musculosa", precio: " $ 4500", categoria: "Indumentaria", talle: "M" },
 ];
 
+const producto = parseInt(prompt("Ingrese el número de ID del producto que desea comprar:"
+    + "\nID1. Remera"
+    + "\nID2. Mochila"
+    + "\nID3. Gorra"
+    + "\nID4. Buzo"
+    + "\nID5. Musculosa"));
 
+const filtrado = productos.filter(item => item.id === producto);
 
+const productoElegido =
 
-while (prompt) {
-    const producto = prompt("Ingrese el nombre del producto que desea comprar:"
-        + "\n1. Remera"
-        + "\n2. Mochila"
-        + "\n3. Gorra"
-        + "\n4. Buzo"
-        + "\n5. Musculosa");
-
-    const filtrado = productos.filter(item => item.nombre === producto.toLowerCase());
-
-    filtrado.forEach((item) => {
+    filtrado.find((item) => {
         let mensaje = `
     Id: ${item.id}
     Nombre: ${item.nombre}
@@ -29,8 +27,15 @@ while (prompt) {
     Categoria: ${item.categoria}
     Talle: ${item.talle}
     `; alert(mensaje);
-    })
+    });
+
+const confirmaCompra = confirm ("¿Desea comprarlo?")
+ if (confirmaCompra) {
+    producto.push(item => item.id === producto)
+
+    alert("producto ingresado al carrito")
 }
+
 
 
 
