ea3d88b2aaac09a337b8093591817e54eb96f924
Santiago Santoro <santiagosantoro10@gmail.com>
2023-02-16 16:04:37 -0300

    cambios segunda reentrega

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
-// CARRITO DE COMPRAS DE LA SECCION MERCH//
-
 const productos = [
-    { id: 1, nombre: "Remera", precio: "$3500", categoria: "Indumentaria", talle: "M" },
-    { id: 2, nombre: "Mochila", precio: "$1500", categoria: "Indumentaria", talle: "M" },
-    { id: 3, nombre: "Gorra", precio: "$3500", categoria: "Indumentaria", talle: "M" },
-    { id: 4, nombre: "Buzo", precio: "$6000", categoria: "Indumentaria", talle: "M" },
-    { id: 5, nombre: "Musculosa", precio: " $ 4500", categoria: "Indumentaria", talle: "M" },
+    { id: 1, nombre: "Remera", precio: 3500, categoria: "Indumentaria", talle: "M" },
+    { id: 2, nombre: "Mochila", precio: 1500, categoria: "Indumentaria", talle: "M" },
+    { id: 3, nombre: "Gorra", precio: 3500, categoria: "Indumentaria", talle: "M" },
+    { id: 4, nombre: "Buzo", precio: 6000, categoria: "Indumentaria", talle: "M" },
+    { id: 5, nombre: "Musculosa", precio: 4500, categoria: "Indumentaria", talle: "M" },
 ];
 
-const producto = parseInt(prompt("Ingrese el número de ID del producto que desea comprar:"
+const carrito = [];
+
+const productoIndex = parseInt(prompt("Ingrese el número de ID del producto que desea comprar:"
     + "\nID1. Remera"
     + "\nID2. Mochila"
     + "\nID3. Gorra"
     + "\nID4. Buzo"
-    + "\nID5. Musculosa"));
+    + "\nID5. Musculosa")) - 1;
 
-const filtrado = productos.filter(item => item.id === producto);
+const productoSeleccionado = productos.filter(producto => producto.id === (productoIndex + 1))[0];
 
-const productoElegido =
-
-    filtrado.find((item) => {
-        let mensaje = `
-    Id: ${item.id}
-    Nombre: ${item.nombre}
-    Precio: ${item.precio}
-    Categoria: ${item.categoria}
-    Talle: ${item.talle}
-    `; alert(mensaje);
-    });
+let mensaje = `
+    Id: ${productoSeleccionado.id}
+    Nombre: ${productoSeleccionado.nombre}
+    Precio: ${productoSeleccionado.precio}
+    Categoria: ${productoSeleccionado.categoria}
+    Talle: ${productoSeleccionado.talle}
+    `;
+alert(mensaje);
 
 const confirmaCompra = confirm ("¿Desea comprarlo?")
- if (confirmaCompra) {
-    producto.push(item => item.id === producto)
-
-    alert("producto ingresado al carrito")
+if (confirmaCompra) {
+    carrito.push(productoSeleccionado);
+    console.log(carrito);
+    alert("Producto ingresado al carrito");
+} else {
+    alert("Compra cancelada");
 }
+
 
 
 
